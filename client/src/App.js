5f710a78aafb60ccdee5fee2051254f004f7b48a
Jenonso <saadiq966@gmail.com>
2023-02-02 23:54:48 -0500

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
+import Admin from './Pages/Admin';
+import Detail from './Pages/Detail';
+import Exam from './Pages/Exam';
+import NavBar from './Components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { useApi } from './hooks/use-api';
 
 function App() {
-  const { response } = useApi();
+    return (
+      <Router>
+        
+        <div className="App">
+            <NavBar />
+            
+        </div>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {response}
-        </p>
-      </header>
-    </div>
-  );
+        <div className='Content'>
+          <Switch>
+            <Route path="/admin">
+                <Admin />
+            </Route>
+            <Route path="/details">
+                <Detail />
+            </Route>
+            <Route path="/exam">
+                <Exam />
+            </Route>
+          </Switch>
+            
+        </div>    
+      </Router>
+    )
+
 }
 
 export default App;
